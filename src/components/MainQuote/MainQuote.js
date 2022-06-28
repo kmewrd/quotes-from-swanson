8726e63f72b7ae09c181aa8d8dd398fcf8ada1fb
Kim Ward <kmewrd@outlook.com>
2022-06-28 11:51:32 -0500

    Fetch quote in useEffect and render

--- src/components/MainQuote/MainQuote.js
+++ src/components/MainQuote/MainQuote.js
@@ -1,9 +1,17 @@
+import { useState, useEffect } from 'react';
+import { getSingleQuote } from '../../apiCalls';
 import './MainQuote.css';
 
 const MainQuote = () => {
+  const [quote, setQuote] = useState('')
+
+  useEffect(() => {
+    getSingleQuote().then(data => setQuote(data[0]))
+  }, [])
+
   return (
     <div>
-      <h2>"Quote goes here..."</h2>
+      <h2>"{quote}"</h2>
       <h3>Ron Swanson</h3>
     </div>
   )
