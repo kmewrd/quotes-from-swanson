a3bdf17cfeb30582c81c02a762f42210090c7c00
Kim Ward <kmewrd@outlook.com>
2022-06-28 11:45:19 -0500

    Create LargeQuote component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LargeQuote from './components/LargeQuote/LargeQuote';
 
 const App = () => {
   return (
     <main className="App">
       <h1>Daily Wisdom from Ron Swanson</h1>
+      <LargeQuote />
     </main>
   );
 }
---
+++ src/components/LargeQuote/LargeQuote.js
@@ -0,0 +1,12 @@
+import './LargeQuote.css';
+
+const LargeQuote = () => {
+  return (
+    <div>
+      <h2>"Quote goes here..."</h2>
+      <h3>Ron Swanson</h3>
+    </div>
+  )
+}
+
+export default LargeQuote;
